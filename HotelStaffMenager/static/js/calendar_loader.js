927038c89c153c7eeff8f579765eff6be0cd51be
Mateusz Skrok <m.p.skrok@gmail.com>
2018-05-28 18:28:58 +0200

    full callendar integration

---
+++ HotelStaffMenager/static/js/calendar_loader.js
@@ -0,0 +1,22 @@
+$(function() {
+
+   var calendar = $('#calendar').fullCalendar('getCalendar');
+
+  // page is now ready, initialize the calendar...
+
+  $('#calendar').fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,basicWeek,basicDay'
+      },
+      navLinks: true, // can click day/week names to navigate views
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events
+      events: {
+        url: "/getReservationList"
+      }
+
+  })
+
+});
